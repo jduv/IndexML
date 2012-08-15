9f60b740ef664463ef2590073d495023f09b8f1f
Jeremy Duvall <matchless.tone@gmail.com>
2012-08-15 13:50:05 -0400

    Style fixes.

--- IndexML/Spreadsheet/CellReference.cs
+++ IndexML/Spreadsheet/CellReference.cs
@@ -45,7 +45,7 @@
         /// <summary>
         /// The alphabet.
         /// </summary>
-        private static readonly char[] alphabet = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+        private static readonly char[] Alphabet = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
 
         /// <summary>
         /// The cell reference.
