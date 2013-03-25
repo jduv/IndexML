5ee8f1abcc3643837eb7df13bbe8fe07bec0daea
Jeremy Duvall <matchless.tone@gmail.com>
2013-03-25 11:27:03 -0400

    Adding missed files

---
+++ IndexML.UnitTests/Test.cs
@@ -0,0 +1,46 @@
+﻿namespace IndexML.UnitTests
+{
+    using System.IO;
+
+    /// <summary>
+    /// A base test class. Defines some re-usable logic.
+    /// </summary>
+    public class Test
+    {
+        #region Fields & Constants
+
+        protected const string TestFilesDir = @"TestFiles\";
+
+        #endregion
+
+        #region Protected Methods
+
+        protected static byte[] ReadAllBytes(string path)
+        {
+            // Helper method, assumes the file exists.
+            var memory = new MemoryStream();
+            using (var fileStream = File.Open(path, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite))
+            {
+                CopyStream(fileStream, memory);
+            }
+
+            return memory.ToArray();
+        }
+
+        protected static void CopyStream(Stream source, Stream target)
+        {
+            // Helper method, assumes source and target are not null.
+            using (source)
+            {
+                var buffer = new byte[32768];
+                int bytesRead;
+                while ((bytesRead = source.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    target.Write(buffer, 0, bytesRead);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ IndexML/Document/DocumentIndexer.cs
@@ -0,0 +1,29 @@
+﻿namespace IndexML.Document
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using DocumentFormat.OpenXml.Wordprocessing;
+
+    /// <summary>
+    /// OpenXML utility class for Indexing word documents.
+    /// </summary>
+    public class DocumentIndexer
+    {
+
+        #region Constructors & Destructors
+
+        public DocumentIndexer(DocPart toIndex)
+        {
+        }
+
+        #endregion
+
+        #region Properties
+
+        public Document Document { get; private set; }
+
+        #endregion
+    }
+}
