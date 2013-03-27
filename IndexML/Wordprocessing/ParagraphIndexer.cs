9bdfc8fdbf313fa5f2cc99ec875537f073c2170f
Jeremy Duvall <matchless.tone@gmail.com>
2013-03-27 12:24:18 -0400

    Adding files

---
+++ IndexML/Wordprocessing/ParagraphIndexer.cs
@@ -0,0 +1,18 @@
+﻿namespace IndexML.Wordprocessing
+{
+    using DocumentFormat.OpenXml.Wordprocessing;
+
+    /// <summary>
+    /// TODO: Implement me.
+    /// </summary>
+    public class ParagraphIndexer
+    {
+        private Paragraph paragraph;
+
+        public ParagraphIndexer(DocumentFormat.OpenXml.Wordprocessing.Paragraph x)
+        {
+            // TODO: Complete member initialization
+            this.paragraph = x;
+        }
+    }
+}
---
+++ IndexML/Wordprocessing/TableIndexer.cs
@@ -0,0 +1,18 @@
+﻿namespace IndexML.Wordprocessing
+{
+    using DocumentFormat.OpenXml.Wordprocessing;
+
+    /// <summary>
+    /// TODO: Implement me.
+    /// </summary>
+    public class TableIndexer
+    {
+        private Table table;
+
+        public TableIndexer(Table table)
+        {
+            // TODO: Complete member initialization
+            this.table = table;
+        }
+    }
+}
