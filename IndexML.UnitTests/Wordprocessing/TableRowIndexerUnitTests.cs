fbbdb3126ab703dd97018786f255c8331ce58c94
Jeremy Duvall <matchless.tone@gmail.com>
2013-04-04 22:43:00 -0400

    Adding missed files

---
+++ IndexML.UnitTests/Wordprocessing/TableRowIndexerUnitTests.cs
@@ -0,0 +1,40 @@
+﻿namespace IndexML.UnitTests.Wordprocessing
+{
+    using System;
+    using DocumentFormat.OpenXml.Wordprocessing;
+    using IndexML.Wordprocessing;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    
+    [TestClass]
+    public class TableRowIndexerUnitTests : WordprocessingDocumentTest
+    {
+        #region Test Methods
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_NullArgument_ThrowsException()
+        {
+            var target = new TableRowIndexer(null);
+        }
+
+        [TestMethod]
+        public void ImplicitCast_ValidIndexer_SameReference()
+        {
+            var expected = new TableRow();
+            var indexer = new TableRowIndexer(expected);
+            var target = (TableRow)indexer;
+
+            Assert.AreSame(expected, indexer.Row);
+        }
+
+        [TestMethod]
+        public void ImplicitCast_Null_IsNull()
+        {
+            TableRowIndexer indexer = null;
+            var target = (TableRow)indexer;
+            Assert.IsNull(target);
+        }
+
+        #endregion
+    }
+}
